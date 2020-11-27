773d77afbeb15152098f966318e72ac1310a129f
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-11-27 14:51:02 +0100

    base

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,9 @@
 <template>
     <q-layout view="lHh Lpr lFf">
-        <q-header elevated></q-header>
+        <q-header elevated>
+            <q-list></q-list>
+        </q-header>
+
         <q-page-container>
             <router-view />
         </q-page-container>
