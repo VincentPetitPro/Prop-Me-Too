2b75ba3cabe6cae13f22352f6e3085f77e4385bb
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-11-27 18:35:37 +0100

    ajout du MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,25 @@
 <template>
     <q-layout view="lHh Lpr lFf">
-        <q-header elevated>
-            <q-list></q-list>
+        <q-header elevated class="bg-dark text-font">
+            <div class="row justify-around">
+                <q-img src="../assets/info.png" style="width:10vw"></q-img>
+                <q-img
+                    src="../assets/nourriture.png"
+                    style="width:10vw"
+                ></q-img>
+                <q-img
+                    src="../assets/douche.png"
+                    style="width:10vw"
+                ></q-img>
+                <q-img
+                    src="../assets/toilette.png"
+                    style="width:10vw"
+                ></q-img>
+                <q-img
+                    src="../assets/necessite.png"
+                    style="width:10vw"
+                ></q-img>
+            </div>
         </q-header>
 
         <q-page-container>
