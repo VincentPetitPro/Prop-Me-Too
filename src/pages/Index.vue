243864c118886dd3e208b7ac94ccf2a205f38c93
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2021-01-02 19:53:44 +0100

    adding markers to Index.vue map

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,44 @@
 <template>
     <q-page class="flex flex-center">
         <gmap-map
-            id="map"
-            :center="center"
-            :zoom="zoom"
-            :map-type-id="mapTypeId"
+            :center="this.$data.center"
+            :zoom="15"
+            :options="{
+                fullscreenControl: false,
+                disableDefaultUi: true,
+                streetViewControl: false
+            }"
         ></gmap-map>
+        <gmap-marker
+            v-for="(m, index) in this.$data.markers"
+            :key="index"
+            :position="m.position"
+        ></gmap-marker>
     </q-page>
 </template>
 
 <script>
-import GMap from "components/GMap.vue";
-
 export default {
     name: "PageIndex",
     data() {
         return {
             center: { lat: 48.85450510484693, lng: 2.3465948626550723 },
-            zoom: 7,
-            mapTypeId: "terrain"
+            markers: [
+                {
+                    position: { lat: 10.0, lng: 10.0 }
+                },
+                {
+                    position: { lat: 11.0, lng: 11.0 }
+                }
+            ]
         };
     }
-    //components: { "g-map": GMap }
 };
 </script>
 
 <style scoped>
-#map {
+.vue-map-container {
+    height: 92.5vh;
     width: 100vw;
-    height: 100vh;
 }
 </style>
