328f6f20c04726d185676b6a5f2942caaaa13f25
vbonneff <victor.bonneff@efrei.net>
2021-01-03 21:08:31 +0100

    commencement nourriture

---
+++ src/pages/Nourriture.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-page class="flex flex-center">
+        <gmap-map
+            :center="this.$data.center"
+            :zoom="15"
+            :options="{
+                fullscreenControl: false,
+                disableDefaultUi: true,
+                streetViewControl: false
+            }"
+            ><gmap-marker
+                v-for="(m, index) in this.$data.markers"
+                :key="index"
+                :position="m.position"
+            ></gmap-marker
+        ></gmap-map>
+    </q-page>
+</template>
+
+<script>
+export default {
+    name: "PageIndex",
+    data() {
+        return {
+            center: { lat: 48.85450510484693, lng: 2.3465948626550723 },
+            markers: [
+                {
+                    position: { lat: 10.0, lng: 10.0 }
+                },
+                {
+                    position: { lat: 11.0, lng: 11.0 }
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+.vue-map-container {
+    height: 92.5vh;
+    width: 100vw;
+}
+</style>
