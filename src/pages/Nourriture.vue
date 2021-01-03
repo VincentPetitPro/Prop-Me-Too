5c74f928762982b931449ca49cef7dc528af56c6
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2021-01-03 21:15:16 +0100

    merge

--- src/pages/Nourriture.vue
+++ src/pages/Nourriture.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <q-page class="flex flex-center">
         <gmap-map
             :center="this.$data.center"
@@ -16,15 +15,11 @@
             ></gmap-marker
         ></gmap-map>
     </q-page>
-=======
-    <q-page></q-page>
->>>>>>> 9710ce045a9d1cf860df4db9f27c10dd79930159
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-    name: "PageIndex",
+    name: "Nourriture",
     data() {
         return {
             center: { lat: 48.85450510484693, lng: 2.3465948626550723 },
@@ -37,22 +32,13 @@ export default {
                 }
             ]
         };
-=======
-    name: "Nourriture",
-    data() {
-        return {};
->>>>>>> 9710ce045a9d1cf860df4db9f27c10dd79930159
     }
 };
 </script>
 
-<<<<<<< HEAD
 <style scoped>
 .vue-map-container {
     height: 92.5vh;
     width: 100vw;
 }
 </style>
-=======
-<style scoped></style>
->>>>>>> 9710ce045a9d1cf860df4db9f27c10dd79930159
